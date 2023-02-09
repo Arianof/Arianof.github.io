5114e8aa5815a7d0b34f42aa3026232529508bbe
Arianof <Arian.fathi03@gmai.com>
2023-02-09 14:10:32 +0330

    update: react router removed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RateForm from "./components/rateForm";
 import Submit from "./components/Submit";
 function App() {
   return (
       <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<RateForm/>}/>
-            <Route path="submitted" element={<Submit/>}/>
-          </Routes>
-        </BrowserRouter>
+        <RateForm/>
       </>
   );
 }
